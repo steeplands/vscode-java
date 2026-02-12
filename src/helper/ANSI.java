87ee24a66cb5bca500cec74eb8f9f11e01fb2f88
flan6002 <fand@gmx.at>
2026-02-12 18:28:57 +0100

    Add background color and text formatting constants to ANSI class

--- src/helper/ANSI.java
+++ src/helper/ANSI.java
@@ -10,4 +10,19 @@ public static final String ANSI_BLUE = "\u001B[34m";
 public static final String ANSI_PURPLE = "\u001B[35m";
 public static final String ANSI_CYAN = "\u001B[36m";
 public static final String ANSI_WHITE = "\u001B[37m";
+
+public static final String BACK_BLACK = "\033[40m";
+public static final String BACK_RED = "\033[41m";
+public static final String BACK_GREEN = "\033[42m";
+public static final String BACK_YELLOW = "\033[43m";
+public static final String BACK_BLUE = "\033[44m";
+public static final String BACK_PURPLE = "\033[45m";
+public static final String BACK_CYAN = "\033[46m";
+public static final String BACK_WHITE = "\033[47m";
+
+public static final String NORMAL = "\033[0m";
+public static final String BOLD = "\033[1m";
+public static final String UNDERLINE = "\033[4m";
+public static final String BLINKY = "\033[5m";
+public static final String FLIPPED = "\033[7m";
 }
