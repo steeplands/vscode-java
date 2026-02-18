3d2974aa0c5a02ef914959779013b63d52dd46c8
flan6002 <f.and@gmx.at>
2026-02-18 09:10:44 +0100

    Refactor ReadTextfile to improve file reading logic and remove redundant code

--- src/files/ReadTextfile.java
+++ src/files/ReadTextfile.java
@@ -8,17 +8,19 @@ public class ReadTextfile {
         String filepath = "resources/smiley.txt";
         try {
             FileReader reader = new FileReader(filepath);
+
+            while (true) {
+                int buffer = reader.read();
+                if (buffer == -1) {
+                    break;
+                }
+                System.out.print((char)buffer);
+            }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        while (true) {
-            int buffer = reader.read();
-            if (buffer == -1) {
-                break;
-            }
-            System.out.print(buffer);
-        }
+        
     }
 }
