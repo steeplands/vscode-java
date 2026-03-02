fa33e6b8114054679b6e53a9d4d9f8f2e062c193
flan6002 <f.and@gmx.at>
2026-03-02 15:06:01 +0100

    Implement text analysis and result saving in TextAnalyzer class

--- src/files/TextAnalyzer.java
+++ src/files/TextAnalyzer.java
@@ -1,7 +1,9 @@
 package files;
 
 import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 
 // a = 97 | z = 122
 // A = 65 | Z = 90
@@ -13,12 +15,12 @@ public class TextAnalyzer {
         }
         return c;
     }
-    public static int[] analyzeText(String filename) {
 
+    public static int[] analyzeText(String fileName) {
         int[] table = new int[26];
 
         try {
-            FileReader reader = new FileReader(filename);
+            FileReader reader = new FileReader(fileName);
             boolean run = true;
             while (run) {
                 int cbuf = reader.read();
@@ -36,8 +38,37 @@ public class TextAnalyzer {
         }
         return table;
     }
+
+    public static void saveResult(String fileName) {
+        String fileNameOut = fileName + ".out";
+        try {
+            BufferedWriter bWriter = new BufferedWriter(new FileWriter(fileNameOut));
+
+            bWriter.write("Analyzed text: " + fileName);
+            bWriter.newLine();
+
+            int[] data = analyzeText(fileName);
+            
+            char abc = 'A';
+            for (int i = 0; i < data.length; i++) {
+                if (data[i] > 0) {
+                    bWriter.newLine();
+                    bWriter.write((char)(abc + i) + " | " + data[i]);
+                }
+            }
+
+            bWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        int[] result = analyzeText("res/smiley.txt");
+        final String PATH = "res/smiley.txt";
+
+        int[] result = analyzeText(PATH);
+        saveResult(PATH);
+        
         char abc = 'A';
         for (int i : result) {
             System.out.printf("%s | %d\n", abc++, i);
