af0b216f0b1d3958e41936d58101ffa09522f45e
flan6002 <fand@gmx.at>
2026-03-18 18:35:46 +0100

    first oop things uploaded

---
+++ src/oop/Rechteck.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class Rechteck {
+    double laenge;
+    double breite;
+
+    /*public void printState {
+        System.out.println("Länge: " + laenge);
+        System.out.println("Breite: " + breite);
+    }
+    */
+}
---
+++ src/oop/Tester.java
@@ -0,0 +1,19 @@
+package oop;
+
+public class Tester {
+    public static void main(String[] args) {
+        Rechteck recht1 = new Rechteck();
+        recht1.laenge = 5;
+        recht1.breite = 3;
+
+        System.out.println("Länge: " + recht1.laenge);
+        System.out.println("Breite: " + recht1.breite);
+
+        Rechteck recht2 = new Rechteck();
+        recht2.laenge = 8;
+        recht2.breite = 2.5;
+
+        System.out.println("Länge: " + recht2.laenge);
+        System.out.println("Breite: " + recht2.breite);
+    }
+}
